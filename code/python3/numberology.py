2a89af9598289b78888962dd3dddbe8c6bdf1233
Isaac Halvorson <hello@hisaac.net>
2018-03-12 13:54:33 -0500

    Trim up the code some

--- code/python3/numberology.py
+++ code/python3/numberology.py
@@ -1,53 +1,62 @@
-def get_word_number(word):
-	wordnum = ""
+# numberology.py by Isaac Halvorson
+#
+# A Python 3 implementation of simple Numerology algorithms
 
-	for character in word:
-		wordnum += str(get_character_number(character))
 
-	return int(wordnum)
+#def get_word_number(word):
+#	wordnum = ""
+#
+#	for character in word:
+#		wordnum += str(get_character_number(character))
+#
+#	return int(wordnum)
+#
+#
+#def get_character_number(character):
+#	if character.isnumeric():
+#		return character
+#
+#	if character.isalpha():
+#		character = character.lower()
+#		return string.index(character)
+#		
+##		ord(character - 96)
+#
+#
+#def find_digital_root(word):
+#	wordnum = get_word_number(word)
+#	return (wordnum - 1) % 9 + 1
+
+def calculate_final_number(number_string):
+	return (int(number_string) - 1) % 9 + 1
 
 
 def get_character_number(character):
-	if character.isalnum():
-		character = character.lower()
+	if character.isnumeric():
+		return character
+	elif character.isalpha():
+		return ord(character.lower()) - 96
 	else:
 		return 0
 
-	if character.isnumeric():
-		return character
 
-	if character == "a" or character == "j" or character == "s":
-		return 1
-	if character == "b" or character == "k" or character == "t":
-		return 2
-	if character == "c" or character == "l" or character == "u":
-		return 3
-	if character == "d" or character == "m" or character == "v":
-		return 4
-	if character == "e" or character == "n" or character == "w":
-		return 5
-	if character == "f" or character == "o" or character == "x":
-		return 6
-	if character == "g" or character == "p" or character == "y":
-		return 7
-	if character == "h" or character == "q" or character == "z":
-		return 8
-	if character == "i" or character == "r":
-		return 9
-
-
-def find_digital_root(word):
-	wordnum = get_word_number(word)
-	return (wordnum - 1) % 9 + 1
+def numberology(text):
+	big_number_string = ""
+	
+	for character in text:
+		big_number_string += str(get_character_number(character))
+	
+	return calculate_final_number(big_number_string)
 
 
 def main():
-	birthday = input("Please enter your birthday: ")
 	name = input("Please enter your full name: ")
-	life_number = find_digital_root(birthday)
-	name_number = find_digital_root(name)
-	print("Your life number is:", life_number)
+	name_number = numberology(name)
 	print("Your name number is:", name_number)
+
+	birthday = input("Please enter your birthday: ")
+	life_number = numberology(birthday)
+	print("Your life number is:", life_number)
 
 
 if __name__ == "__main__":
